4cf72151c9d975bad4eb17dee5b1b3590b665e27
adkhamjonDev <adkhamjon.rakhimov.dev@gmail.com>
2021-10-09 17:10:52 +0500

    committed

--- app/src/main/java/uz/adkhamjon/fooddelivery/fragments/MainLoginFragment.kt
+++ app/src/main/java/uz/adkhamjon/fooddelivery/fragments/MainLoginFragment.kt
@@ -6,39 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.tabs.TabLayout
 import uz.adkhamjon.fooddelivery.FoodActivity
-import uz.adkhamjon.fooddelivery.R
-import uz.adkhamjon.fooddelivery.SplashActivity
-import uz.adkhamjon.fooddelivery.adapters.MyPagerAdapter
 import uz.adkhamjon.fooddelivery.databinding.FragmentMainLoginBinding
+import android.text.Editable
+
+import android.text.TextWatcher
+import android.widget.Toast
+
 
 class MainLoginFragment : Fragment() {
     private lateinit var binding: FragmentMainLoginBinding
-    private lateinit var myPagerAdapter: MyPagerAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding=FragmentMainLoginBinding.inflate(inflater, container, false)
-//        myPagerAdapter= MyPagerAdapter(requireContext(),childFragmentManager,binding.tablayout.tabCount)
-//        binding.viewPager.adapter=myPagerAdapter
-//        binding.viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding.tablayout))
-//        binding.tablayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-//            override fun onTabSelected(tab: TabLayout.Tab) {
-//                binding.viewPager.currentItem = tab.position
-//            }
-//            override fun onTabUnselected(tab: TabLayout.Tab) {
-//
-//            }
-//            override fun onTabReselected(tab: TabLayout.Tab) {
-//
-//            }
-//        })
-//        binding.viewPager.setOnTouchListener(View.OnTouchListener { v, event -> true })
+
 
         binding.loginBtn.setOnClickListener {
             startActivity(Intent(requireContext(), FoodActivity::class.java))
+            activity?.finish()
+//            val toString = binding.code.text.toString()
+//            Toast.makeText(requireContext(), toString, Toast.LENGTH_SHORT).show()
         }
         return binding.root
     }
